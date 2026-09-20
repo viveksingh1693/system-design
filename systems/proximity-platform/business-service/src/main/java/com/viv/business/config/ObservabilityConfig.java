54b1d933654024265305cc62e014b437d284f87d
vivesing <vivek.singh1694@gmail.com>
2026-09-20 17:04:49 +0530

    [proximity-platform] adding changes for observablity

--- systems/proximity-platform/business-service/src/main/java/com/viv/business/config/ObservabilityConfig.java
+++ systems/proximity-platform/business-service/src/main/java/com/viv/business/config/ObservabilityConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 
-@Configuration 
+@Configuration
 public class ObservabilityConfig {
 
-    @Bean 
-    ObservationRegistry observationRegistry() {
-        return ObservationRegistry.create();
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
     }
 }
